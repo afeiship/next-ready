bc5815d53763c012e3ca4b52462353a188af76aa
afeiship <1290657123@qq.com>
2017-10-14 21:48:52 +0800

    issue: dom loaded const

--- dist/next-ready.js
+++ dist/next-ready.js
@@ -3,12 +3,13 @@
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
   var readyRE = /complete|loaded|interactive/;
+  var DOM_LOADED = 'DOMContentLoaded';
 
   nx.ready = function (inCallback) {
     if (readyRE.test(document.readyState) && document.body) {
       inCallback();
     } else {
-      document.addEventListener('DOMContentLoaded', function () {
+      document.addEventListener(DOM_LOADED, function () {
         inCallback();
       }, false);
     }
